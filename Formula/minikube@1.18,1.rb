23b6d582e0b3a171d0bc8a188aa6d93d93781438
Urs Rau (INT) <urs.rau@om.org>
2021-04-22 18:49:36 +0200

    added minikube v 1.18.1

---
+++ Formula/minikube@1.18,1.rb
@@ -0,0 +1,37 @@
+class MinikubeAT118,1 < Formula
+  desc "Run a Kubernetes cluster locally"
+  homepage "https://minikube.sigs.k8s.io/"
+  url "https://github.com/kubernetes/minikube.git",
+      tag:      "v1.18.1",
+      revision: "09ee84d530de4a92f00f1c5dbc34cead092b95bc"
+  license "Apache-2.0"
+  head "https://github.com/kubernetes/minikube.git"
+
+  depends_on "go" => :build
+  depends_on "go-bindata" => :build
+  depends_on "kubernetes-cli"
+
+  def install
+    system "make"
+    bin.install "out/minikube"
+
+    output = Utils.safe_popen_read("#{bin}/minikube", "completion", "bash")
+    (bash_completion/"minikube").write output
+
+    output = Utils.safe_popen_read("#{bin}/minikube", "completion", "zsh")
+    (zsh_completion/"_minikube").write output
+  end
+
+  test do
+    output = shell_output("#{bin}/minikube version")
+    assert_match "version: v#{version}", output
+
+    (testpath/".minikube/config/config.json").write <<~EOS
+      {
+        "vm-driver": "virtualbox"
+      }
+    EOS
+    output = shell_output("#{bin}/minikube config view")
+    assert_match "vm-driver: virtualbox", output
+  end
+end
